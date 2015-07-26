d96ea2bae32ec4389a9c118664d59456f4bb389e
brandonkehl <kehl.brandon@gmail.com>
2015-07-25 23:34:24 -0500

    Added all code
    Add user input soon

---
+++ main.c
@@ -0,0 +1,44 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by brandon kehl on 7/24/15.
+//  Copyright (c) 2015 bignerdranch. All rights reserved.
+//
+
+#include <stdio.h>
+//Here is the declaration of the type Person
+    typedef struct{
+        float heightInMeters;
+        int weightInKilos;
+    
+    }  Person;
+float bodyMassIndex(Person p){
+
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+
+}
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    Person mikey;
+    mikey.heightInMeters = 1.7;
+    mikey.weightInKilos = 96;
+    
+    Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    float bmi;
+    bmi = bodyMassIndex(mikey);
+    printf("mikey has a BMI of %.2f\n", bmi);
+    
+    bmi = bodyMassIndex(aaron);
+    printf("aaron has a BMI of %.2f\n", bmi);
+    return 0;
+
+}
